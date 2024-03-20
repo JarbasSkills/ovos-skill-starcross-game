19e0eab1bb52cd5673389760a5706c398de20b02
miro <jarbasai@mailfence.com>
2024-03-20 19:09:56 +0000

    modernize

--- __init__.py
+++ __init__.py
@@ -1,24 +1,15 @@
 from pyfrotz.ovos import FrotzSkill
 from ovos_workshop.decorators import conversational_intent, intent_handler
+from pyfrotz.parsers import starcross_intro_parser
 
 
 class StarCrossSkill(FrotzSkill):
     def __init__(self, *args, **kwargs):
-
-        # Game specific init
-        def intro_parser(game):
-            intro = """you are sound asleep in your bunk aboard the deep-space black hole prospecting ship "starcross," operating out of ceres.
-            just as your sleep becomes deep and comfortable, an alarm bell begins ringing! it's the mass detector! instantly you awake.
-            this hasn't been a profitable trip so far, and you don't even have the cash for repairs.
-            this could be the break you've been waiting for."""
-            game._frotz_read()
-            return intro
-
         # game is english only, apply bidirectional translation
         super().__init__(game_id="starcross",
                          game_lang="en-us",
                          game_data=f'{self.root_dir}/res/{self.game_id}.z5',
-                         intro_parser=intro_parser,
+                         intro_parser=starcross_intro_parser,
                          *args, **kwargs)
 
     @intent_handler("play.intent")

b0878f160d6c96e13d005518c9c4133287f4d56f
miro <jarbasai@mailfence.com>
2024-12-18 17:35:42 +0000

    frotz

--- __init__.py
+++ __init__.py
@@ -1,30 +1,13 @@
 from pyfrotz.ovos import FrotzSkill
-from ovos_workshop.decorators import conversational_intent, intent_handler
 from pyfrotz.parsers import starcross_intro_parser
 
 
 class StarCrossSkill(FrotzSkill):
     def __init__(self, *args, **kwargs):
         # game is english only, apply bidirectional translation
-        super().__init__(game_id="starcross",
+        super().__init__(*args,
+                         game_id="starcross",
                          game_lang="en-us",
                          game_data=f'{self.root_dir}/res/{self.game_id}.z5',
                          intro_parser=starcross_intro_parser,
-                         *args, **kwargs)
-
-    @intent_handler("play.intent")
-    def handle_play(self, message=None):
-        self.start_game(load_save=True)
-
-    # intents
-    @conversational_intent("exit.intent")
-    def handle_exit(self, message=None):
-        self.exit_game()
-
-    @conversational_intent("restart_game.intent")
-    def handle_restart(self, message=None):
-        self.start_game(load_save=False)
-
-    @conversational_intent("save.intent")
-    def handle_save(self, message=None):
-        self.save_game()
+                         **kwargs)
